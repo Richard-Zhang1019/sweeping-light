fb2bb050f173486e9cff3cec70dab0f9bca972b5
Richard Zhang <1501109542@qq.com>
2023-03-12 19:51:07 +0800

    feat: finish

--- src/components/SweepingLight.tsx
+++ src/components/SweepingLight.tsx
@@ -2,10 +2,26 @@ import { Box, keyframes } from '@chakra-ui/react'
 import { FC } from 'react'
 
 interface SweepingLightProps {
-  children?: React.ReactNode
+  // 包裹的子元素
+  children: React.ReactNode
+  // 线的宽度
+  lightWidth?: number
+  // 线的颜色
+  lightBg?: string
+  // 线的旋转角度
+  lightRotate?: number
+  // 动画持续时间
+  duration?: number
 }
 
-const SweepingLight: FC<SweepingLightProps> = ({ children }) => {
+// 封装为通用组件
+const SweepingLight: FC<SweepingLightProps> = ({
+  children,
+  lightWidth = 80,
+  lightBg = 'linear-gradient(90deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, .24) 50.04%, rgba(255, 255, 255, 0) 99.37%)',
+  lightRotate = 22.5,
+  duration = 2.4
+}) => {
   const sweep = keyframes`
     from { transform: translateX(-100%)}
     to { transform: translateX(200%) }
@@ -16,24 +32,24 @@ const SweepingLight: FC<SweepingLightProps> = ({ children }) => {
     <Box
       position="relative"
       overflow="hidden"
-      transition="opacity 300"
+      transition="opacity 300ms"
       opacity={1}
     >
-      {/*  */}
       <Box
         position="absolute"
         top={0}
         left={0}
-        animation={`${sweep} 1.3s linear infinite`}
+        animation={`${sweep} 2.4s infinite linear`}
         h="100%"
         w="100%"
+        style={{ animationDuration: `${duration}s` }}
       >
         {/* 扫动的光线 */}
         <Box
           h="100%"
-          bgColor="#f1f1f1"
-          w={80}
-          transform="rotate(22.5deg) scaleY(2)"
+          w={lightWidth}
+          bg={lightBg}
+          transform={`rotate(${lightRotate}deg) scaleY(2)`}
           transformOrigin="top right"
         />
       </Box>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -20,12 +20,18 @@ const About = () => {
       <h1>Yu-React-template</h1>
       <Box>redux count: {value}</Box>
       <SweepingLight>
-        123
-        <h1>Yu-React-template</h1>
-        <h1>Yu-React-template</h1>
-        <h1>Yu-React-template</h1>
-        <h1>Yu-React-template</h1>
-        <h1>Yu-React-template</h1>
+        <Box bgColor='#ddd' h={20} w={200} rounded="20px" m={5} />
+        <Box bgColor='#ddd' h={20} w={200} rounded="20px" m={5} />
+        <Flex>
+          <Box bgColor='#ddd' h={100} w={92} rounded="20px" m={5} />
+          <Box bgColor='#ddd' h={100} w={93} rounded="20px" m={5} />
+        </Flex>
+        <Box bgColor='#ddd' h={20} w={200} rounded="20px" m={5} />
+        <Box bgColor='#ddd' h={20} w={200} rounded="20px" m={5} />
+        <Flex>
+          <Box bgColor='#ddd' h={100} w={92} rounded="20px" m={5} />
+          <Box bgColor='#ddd' h={100} w={93} rounded="20px" m={5} />
+        </Flex>
       </SweepingLight>
     </Flex>
   )
